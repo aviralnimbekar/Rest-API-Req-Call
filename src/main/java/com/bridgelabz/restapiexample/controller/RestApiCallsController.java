37af780bb3d803a2f1c403511bd159b3b4804d29
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-10-08 00:43:39 +0530

    ADDED : Printing message with QueryParam

--- src/main/java/com/bridgelabz/restapiexample/controller/RestApiCallsController.java
+++ src/main/java/com/bridgelabz/restapiexample/controller/RestApiCallsController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.restapiexample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class RestApiCallsController {
     @GetMapping("/")
     public String sayHello() {
         return "Hello from BridgeLabz. This is Hello message from Rest call";
+    }
+
+    @GetMapping("/queryParam")
+    public String getParamMessage(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz. This is Param message";
     }
 }
